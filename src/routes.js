1f4e0d331e911da1001c9412051140cb0e5ea08a
faridhr <faridhidayaturrahman@gmail.com>
2021-06-16 14:14:18 +0700

    #4 Commit (Finally)

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,10 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -14,7 +20,17 @@ const routes = [
   {
     method: 'GET',
     path: '/notes/{id}',
-    handler: () => getNoteByIdHandler,
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 ];
 
